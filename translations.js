87ff3de5a7babd4fda8139a265af23d434ce36f7
bkseatown <bkseatown@gmail.com>
2026-02-03 10:03:34 +0800

    Delete translations.js

--- translations.js
+++
@@ -1,8 +0,0 @@
-// Translations.js - Minimal placeholder
-// This file prevents "translations not loaded" errors
-
-window.TRANSLATIONS = {
-    // Add translations here if needed in the future
-};
-
-console.log('✓ Translation system ready');
